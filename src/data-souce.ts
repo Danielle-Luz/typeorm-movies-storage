24c7902cd434a6f5d909cb68b2b35ca5f0d5399a
Danielle-Luz <daniln2209@gmail.com>
2023-02-23 22:15:44 -0300

    feat: create data source config file

--- src/data-souce.ts
+++ src/data-souce.ts
@@ -1,8 +1,28 @@
-import path from "path"
+import path from "path";
+import { DataSourceOptions } from "typeorm";
 
-export const dataSourceConfig = () => {
+export const dataSourceConfig = ((): DataSourceOptions => {
   const entitiesPath = path.join(__dirname, "./entities/**.{js,ts}");
   const migrationsPath = path.join(__dirname, "./migrations/**.{js,ts}");
 
-  
-}   
+  if (process.env.NODE_ENV === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  if (!process.env.DATABASE_URL)
+    throw new Error("Insert a value in the DATABASE_URL enviroment variable");
+
+  return {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: true,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+})();
