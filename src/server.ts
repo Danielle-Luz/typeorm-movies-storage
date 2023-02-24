e77ccb37f21984691339dc6e222c6d95d09cca91
Danielle-Luz <daniln2209@gmail.com>
2023-02-24 00:15:58 -0300

    feat: implements server.ts

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { api } from "./app";
+import { appDataSource } from "./data-souce";
+
+appDataSource
+  .initialize()
+  .then(async () => {
+    console.log("Database was connected");
+
+    await api.listen(3000, () => {
+      console.log("API is listening D:");
+    });
+  })
+  .catch((error) => console.error(error));
