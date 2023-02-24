9a00992abd8d5ec1542362aefaeab5e0e6fb5c41
Danielle-Luz <daniln2209@gmail.com>
2023-02-24 00:57:14 -0300

    feat: add decorators in movies entity

--- src/entities/Movies.ts
+++ src/entities/Movies.ts
@@ -1,11 +1,21 @@
-import { Entity } from "typeorm";
+import { type } from "os";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 class Movies {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: "varchar", length: 50, unique: true })
   name: string;
+
+  @Column({ type: "text", nullable: true })
   description: string;
+
+  @Column()
   duration: number;
+
+  @Column()
   price: number;
 }
 
--- src/schemas/movies.schemas.ts
+++ src/schemas/movies.schemas.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const moviesSchema = z.object({
   id: z.number(),
   name: z.string().max(50),
-  description: z.string(),
+  description: z.string().optional(),
   duration: z.number().int(),
   price: z.number().int(),
 });
